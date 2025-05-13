5d24adb22b90579d4391d14aa27e1aef8485a9d1
elkingutierrex <elkin.gutierrex@gmail.com>
2025-05-13 12:30:57 -0500

    first commit

---
+++ Demo.java
@@ -0,0 +1,12 @@
+public class Demo
+{
+    public static void main (String a[])
+    {
+        int number = 1;
+        int count = number++;
+        System.out.println("Number " + number);
+        System.out.println("Count " + count);
+
+    }
+
+}
