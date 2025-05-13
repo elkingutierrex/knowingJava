84597d64e7fc14d9f79f668ce576d460e9a005a2
elkingutierrex <elkin.gutierrex@gmail.com>
2025-05-13 12:42:57 -0500

    feat calculator class

--- Demo.java
+++ Demo.java
@@ -1,12 +1,22 @@
+class Calculator
+{
+    public int add (int num1, int num2)
+    {
+        int result = (num1 + num2);
+        return result;
+    }
+}
+
 public class Demo
 {
     public static void main (String a[])
     {
-        int number = 1;
-        int count = number++;
-        System.out.println("Number " + number);
-        System.out.println("Count " + count);
+        int number1 = 7;
+        int number2 = 5;
 
+        Calculator calc = new Calculator();
+        int result = calc.add( number1, number2);
+        System.out.println(result); 
     }
 
 }
